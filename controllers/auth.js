831dab71c12d5797d8202e5fcbfdaa153df0682d
clock-head <james.n.kai92@gmail.com>
2025-03-04 13:31:46 +1000

    save super user delete code

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,20 +16,6 @@ exports.postSignUp = async (req, res, next) => {
   res.redirect('/sign-in');
 };
 
-exports.createSuperuser = async (req, res, next) => {
-  const userDoc = await User.findOne({ email: req.email });
-
-  if (userDoc) {
-    return res.status(400).json({
-      message: 'user exists',
-    });
-  }
-
-  const newUser = await saveSuperUser(req.body);
-
-  res.status(200).json(newUser);
-};
-
 exports.postSignIn = async (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
--- routes/admin.js
+++ routes/admin.js
@@ -2,13 +2,10 @@ const path = require('path');
 const cors = require('cors');
 const express = require('express');
 
-const authController = require('../controllers/auth');
 const articlesController = require('../controllers/blog/article');
 const { isSuperUser } = require('../middleware/is-auth');
 
 const router = express.Router();
-
-router.post('/create-superuser', authController.createSuperuser);
 
 router.post(
   '/submit-article',
